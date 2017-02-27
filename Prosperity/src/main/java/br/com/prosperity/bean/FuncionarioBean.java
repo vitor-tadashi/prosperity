1efb33cbf68c71c5658fdf8234cd8c0a45088317
Leonardo Ramos <drleo111@gmail.com>
2017-02-27 15:23:49 -0300

    FuncionarioBean

--- Prosperity/src/main/java/br/com/prosperity/bean/FuncionarioBean.java
+++ Prosperity/src/main/java/br/com/prosperity/bean/FuncionarioBean.java
@@ -1,5 +1,32 @@
 package br.com.prosperity.bean;
 
 public class FuncionarioBean {
-
+	private int idFuncionario;
+	private String nome;
+	private int idCargo;
+	private int idSenioridade;
+	public int getIdFuncionario() {
+		return idFuncionario;
+	}
+	public void setIdFuncionario(int idFuncionario) {
+		this.idFuncionario = idFuncionario;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getIdCargo() {
+		return idCargo;
+	}
+	public void setIdCargo(int idCargo) {
+		this.idCargo = idCargo;
+	}
+	public int getIdSenioridade() {
+		return idSenioridade;
+	}
+	public void setIdSenioridade(int idSenioridade) {
+		this.idSenioridade = idSenioridade;
+	}
 }
