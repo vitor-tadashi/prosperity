c3a2089ccf802982a88687303a36eae0b04ff185
Leonardo Ramos <drleo111@gmail.com>
2017-02-27 15:15:20 -0300

    ContatoBean

--- Prosperity/src/main/java/br/com/prosperity/bean/ContatoBean.java
+++ Prosperity/src/main/java/br/com/prosperity/bean/ContatoBean.java
@@ -1,5 +1,25 @@
 package br.com.prosperity.bean;
 
 public class ContatoBean {
-
+	private int idContato;
+	private String telefone;
+	private String celular;
+	public int getIdContato() {
+		return idContato;
+	}
+	public void setIdContato(int idContato) {
+		this.idContato = idContato;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getCelular() {
+		return celular;
+	}
+	public void setCelular(String celular) {
+		this.celular = celular;
+	}
 }
