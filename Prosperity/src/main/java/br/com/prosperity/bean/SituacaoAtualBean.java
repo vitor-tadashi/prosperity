9e96efa7f8419492d1f285b316b64e8061be993b
Leonardo Ramos <drleo111@gmail.com>
2017-02-27 15:36:30 -0300

    SituacaoAtualBean

--- Prosperity/src/main/java/br/com/prosperity/bean/SituacaoAtualBean.java
+++ Prosperity/src/main/java/br/com/prosperity/bean/SituacaoAtualBean.java
@@ -1,5 +1,19 @@
 package br.com.prosperity.bean;
 
 public class SituacaoAtualBean {
-
+	private int idSituacaoAtual;
+	private String descricao;
+	
+	public int getIdSituacaoAtual() {
+		return idSituacaoAtual;
+	}
+	public void setIdSituacaoAtual(int idSituacaoAtual) {
+		this.idSituacaoAtual = idSituacaoAtual;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
 }
