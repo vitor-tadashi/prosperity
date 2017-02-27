c99b37a1dfd4d6ba5c30c81f4cb897c2197b800d
Andre Posman <Andre Posman>
2017-02-27 15:36:22 -0300

    Classe Bean: Perfil, Senioridade, TipoCurso

--- Prosperity/src/main/java/br/com/prosperity/bean/TipoCursoBean.java
+++ Prosperity/src/main/java/br/com/prosperity/bean/TipoCursoBean.java
@@ -1,5 +1,33 @@
 package br.com.prosperity.bean;
 
 public class TipoCursoBean {
+	
+	private int id;
+	private String descricao;
+	
+	public TipoCursoBean(int id, String descricao){
+		this.id = id;
+		this.descricao = descricao;
+	}
+
+	public TipoCursoBean(){
+
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}	
 
 }
