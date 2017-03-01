d547b87d65c6b241d5c9410a5d246bc65e78f0b8
Leonardo Ramos <drleo111@gmail.com>
2017-03-01 10:46:21 -0300

    Adicionando Lists nas classes modificadas

--- Prosperity/src/main/java/br/com/prosperity/bean/ContatoBean.java
+++ Prosperity/src/main/java/br/com/prosperity/bean/ContatoBean.java
@@ -4,7 +4,14 @@ public class ContatoBean {
 	private int idContato;
 	private String telefone;
 	private String celular;
+	private CandidatoBean candidato;
 	
+	public CandidatoBean getCandidato() {
+		return candidato;
+	}
+	public void setCandidato(CandidatoBean candidato) {
+		this.candidato = candidato;
+	}
 	public int getIdContato() {
 		return idContato;
 	}
