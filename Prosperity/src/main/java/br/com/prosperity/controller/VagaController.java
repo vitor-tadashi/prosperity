85eca272acfa57b6ad2d75b21bf09273107a1079
PC\Igor <cunha.981@gmail.com>
2017-03-02 13:37:04 -0300

    Correção xml spring

--- Prosperity/src/main/java/br/com/prosperity/controller/VagaController.java
+++ Prosperity/src/main/java/br/com/prosperity/controller/VagaController.java
@@ -2,11 +2,12 @@ package br.com.prosperity.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/")
 public class VagaController {
-	@RequestMapping("/tela")
+	@RequestMapping(method= RequestMethod.GET)
 	public String tela(){
 		return "consultar-vaga";
 	}
