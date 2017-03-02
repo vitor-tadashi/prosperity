39af5fde4ddfaceedbf8c856c0b69fc553e417d2
Stephen Knupfer <stephenrknupfer@hotmail.com>
2017-03-02 14:58:04 -0300

    Configuração do Jetty e Spring MVC

--- Prosperity/src/main/java/br/com/prosperity/controller/VagaController.java
+++ Prosperity/src/main/java/br/com/prosperity/controller/VagaController.java
@@ -1,5 +1,14 @@
 package br.com.prosperity.controller;
 
-public class VagaController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
+@Controller
+public class VagaController {
+	@RequestMapping(value ="/teste", method = RequestMethod.GET)
+	public ModelAndView cliente() {
+		return new ModelAndView("teste");
+	}
 }
