eac711e18857314606922d9e521010cbdfd6c3d2
Fabio <fabio.peres@verity.local>
2017-03-03 17:48:07 -0300

    JPA e alteração na Criação das tabelas

--- Prosperity/src/main/java/br/com/prosperity/entity/EnderecoEntity.java
+++ Prosperity/src/main/java/br/com/prosperity/entity/EnderecoEntity.java
@@ -1,10 +1,43 @@
 package br.com.prosperity.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Entity implementation class for Entity: PerfilFuncionalidadeEntity
+ *
+ */
+@Entity
+@Table(name="tbEndereco")
+
 public class EnderecoEntity {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="idCliente", unique = true)
 	private int id;
+	
+	@Column(name="cep")
 	private String cep;
+	
+	@Column(name="vlNumero")
 	private int numero;
+	
+	@Column(name="dsComplemento")
 	private String complemento;
+	
+	@Column(name="nmEstado")
+	private String estado;
+	
+	@Column(name="nmLogradouro")
+	private String logradouro;
+	
+	@Column(name="nmCidade")
+	private String cidade;
 	
 	public int getId() {
 		return id;
---
+++ Prosperity/src/main/java/br/com/prosperity/entity/RelatorioEntity.java
@@ -0,0 +1,45 @@
+package br.com.prosperity.entity;
+
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: RelatorioEntity
+ *
+ */
+
+@Entity
+@Table(name="tbRelatorio")
+
+public class RelatorioEntity {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="idRelatorio", unique = true)
+	private int id;
+	
+	@Column(name="nmRelatorio")
+	private String nome;
+	
+	@Column(name="cmRelatorio")
+	private String caminho;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCaminho() {
+		return caminho;
+	}
+	public void setCaminho(String caminho) {
+		this.caminho = caminho;
+	}
+
+}
