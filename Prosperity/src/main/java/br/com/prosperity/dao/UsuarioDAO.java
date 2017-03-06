e16b769cfa439dadb5db60ea92a7a62ef224da56
Yuri Bueno <yuri.silva@verity.com.br>
2017-03-06 14:18:57 -0300

    Correção da classe PerfilEntity e Inicio do UsuarioDAO

--- Prosperity/src/main/java/br/com/prosperity/dao/UsuarioDAO.java
+++ Prosperity/src/main/java/br/com/prosperity/dao/UsuarioDAO.java
@@ -1,5 +1,12 @@
 package br.com.prosperity.dao;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
+import br.com.prosperity.bean.UsuarioBean;
+
 
 public class UsuarioDAO {
-
+	
 }
