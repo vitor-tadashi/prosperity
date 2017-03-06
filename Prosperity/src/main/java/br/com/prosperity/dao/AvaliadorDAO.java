5d2a47b42c9a4e81915a1638ee7e3017d9a1460d
André Posman <andre.posman@verity.com.br>
2017-03-06 14:44:24 -0300

    AvaliadorDAO

--- Prosperity/src/main/java/br/com/prosperity/dao/AvaliadorDAO.java
+++ Prosperity/src/main/java/br/com/prosperity/dao/AvaliadorDAO.java
@@ -1,5 +1,24 @@
 package br.com.prosperity.dao;
 
-public class AvaliadorDAO {
+import java.util.List;
 
+import javax.persistence.TypedQuery;
+
+import br.com.prosperity.entity.AvaliadorEntity;
+
+public class AvaliadorDAO extends GenericDAOImpl<AvaliadorEntity, Integer>{
+
+	public AvaliadorEntity buscarAvaliadorPorId(int id){
+		
+		TypedQuery <AvaliadorEntity> query = entityManager
+				.createQuery("SELECT c FROM AvaliadorEntity AS c WHERE c.id = :id AND c.ativo = 'S'", AvaliadorEntity.class);
+		query.setParameter("id", id);
+		List<AvaliadorEntity> listaAvaliador = query.getResultList();
+		if (listaAvaliador.isEmpty()) {
+			return null;
+		} else {
+			return listaAvaliador.get(0);
+		}
+		//return super.obterPorId(id);
+	}
 }
