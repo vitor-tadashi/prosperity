850826de4cc93c52f110d4583737088304487764
Daniel Valle <danielfdovalle@gmail.com>
2017-03-09 14:00:32 -0300

    alterando algumas

--- Prosperity/src/main/java/br/com/prosperity/controller/VagaController.java
+++ Prosperity/src/main/java/br/com/prosperity/controller/VagaController.java
@@ -7,16 +7,16 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping ("vaga")
 public class VagaController {
-	@RequestMapping(value = "/consultar", method = RequestMethod.GET)
+	@RequestMapping(value = "consultar", method = RequestMethod.GET)
 	public String cliente() {
 		return "vaga/consultar-vaga";
 	}
 
-	@RequestMapping(value = "/aprovar", method = RequestMethod.GET)
+	@RequestMapping(value = "aprovar", method = RequestMethod.GET)
 	public String aprovacaoVaga() {
 		return "vaga/aprovacao-vaga";
 	}
-	@RequestMapping (value= "/solicitar", method = RequestMethod.GET)
+	@RequestMapping (value= "solicitar", method = RequestMethod.GET)
 	public String solicitarVaga(){
 		return "vaga/solicitar-vaga";
 	}
