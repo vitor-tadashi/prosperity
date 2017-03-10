b1dbd6a11b25ba0bf334c3ea0f294fbee6b9e646
Stephen Knupfer <stephenrknupfer@hotmail.com>
2017-03-10 00:51:07 -0300

    Logica p/ carregar combo de funcionarios

--- Prosperity/src/main/java/br/com/prosperity/entity/FuncionarioEntity.java
+++ Prosperity/src/main/java/br/com/prosperity/entity/FuncionarioEntity.java
@@ -1,59 +1,33 @@
 package br.com.prosperity.entity;
 
-import java.util.List;
 import javax.persistence.*;
 
 @Entity
 @Table(name = "tbFuncionario")
-
-
 public class FuncionarioEntity {
 	
-	public Integer getId() {
-		return id;
-	}
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	
 	@Column(name="idFuncionario", unique = true, nullable = false)
 	private Integer id;
 	
+	@Column(name="nmFuncionario")
+	private String nome;
+
+	public Integer getId() {
+		return id;
+	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
-	@Column(name="nmFuncionario")
-	private String nome;
-	
-	@ManyToOne (cascade = CascadeType.ALL)
-	@JoinColumn(name="idCargo")
-	private CargoEntity cargo;
-	
-	@ManyToOne (cascade = CascadeType.ALL)
-	@JoinColumn(name="idSenioridade")
-	private SenioridadeEntity senioridade;
-	
-	public int getIdFuncionario() {
-		return id;
-	}
-	public void setIdFuncionario(int idFuncionario) {
-		this.id = idFuncionario;
-	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	public CargoEntity getCargo() {
-		return cargo;
-	}
-	public void setCargo(CargoEntity cargo) {
-		this.cargo = cargo;
-	}
-	public SenioridadeEntity getSenioridade() {
-		return senioridade;
-	}
-	public void setSenioridade(SenioridadeEntity senioridade) {
-		this.senioridade = senioridade;
-	}
+	
 }
