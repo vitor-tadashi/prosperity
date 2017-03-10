ac34ca2f082be2c87651860350f914ab43394d18
Stephen Knupfer <stephenrknupfer@hotmail.com>
2017-03-10 00:54:37 -0300

    Correção na classe FuncionarioEntity

--- Prosperity/src/main/java/br/com/prosperity/entity/FuncionarioEntity.java
+++ Prosperity/src/main/java/br/com/prosperity/entity/FuncionarioEntity.java
@@ -1,6 +1,14 @@
 package br.com.prosperity.entity;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "tbFuncionario")
@@ -13,6 +21,14 @@ public class FuncionarioEntity {
 	
 	@Column(name="nmFuncionario")
 	private String nome;
+	
+	@ManyToOne (cascade = CascadeType.ALL)
+	@JoinColumn(name="idCargo")
+	private CargoEntity cargo;
+	
+	@ManyToOne (cascade = CascadeType.ALL)
+	@JoinColumn(name="idSenioridade")
+	private SenioridadeEntity senioridade;
 
 	public Integer getId() {
 		return id;
@@ -28,6 +44,22 @@ public class FuncionarioEntity {
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+
+	public CargoEntity getCargo() {
+		return cargo;
+	}
+
+	public void setCargo(CargoEntity cargo) {
+		this.cargo = cargo;
+	}
+
+	public SenioridadeEntity getSenioridade() {
+		return senioridade;
+	}
+
+	public void setSenioridade(SenioridadeEntity senioridade) {
+		this.senioridade = senioridade;
 	}
 	
 }
