6fe85c67cd9ddc7c5e5efaaf2a36244c5c85a6db
Thamires Miranda <thamimiranda@live.com>
2017-03-13 16:58:15 -0300

    Alteração  cadastro candidato

--- Prosperity/src/main/java/br/com/prosperity/business/SenioridadeBusiness.java
+++ Prosperity/src/main/java/br/com/prosperity/business/SenioridadeBusiness.java
@@ -14,21 +14,28 @@ import br.com.prosperity.entity.SenioridadeEntity;
 
 @Component
 public class SenioridadeBusiness {
-	
+
 	@Autowired
 	private SenioridadeDAO senioridadeDAO;
-	
+
 	@Autowired
 	private List<SenioridadeBean> senioridadeBean;
-		
+
 	@Autowired
 	private SenioridadeConverter senioridadeConverter;
-	
+
 	@Transactional
-	public List<SenioridadeBean> obterTodos(){
+	public List<SenioridadeBean> obterTodos() {
 		List<SenioridadeEntity> senioridadeEntity = senioridadeDAO.listar();
 		senioridadeBean = senioridadeConverter.convertEntityToBean(senioridadeEntity);
 		return senioridadeBean;
+	}
+
+	@Transactional
+	public List<SenioridadeBean> getSenioridade() {
+		List<SenioridadeBean> senioridadeVaga = senioridadeConverter.convertEntityToBean(senioridadeDAO.listar());
+
+		return senioridadeVaga;
 	}
 
 }
