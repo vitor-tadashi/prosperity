0e1559c9554cb607be88326153f2c1d4fd592807
graziferreira <grazi.ferreira907@gmail.com>
2017-03-14 14:26:31 -0300

    Alteracao na Bean, Converter e Entity da Candidato

--- Prosperity/src/main/java/br/com/prosperity/entity/CandidatoEntity.java
+++ Prosperity/src/main/java/br/com/prosperity/entity/CandidatoEntity.java
@@ -17,6 +17,8 @@ import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
+import br.com.prosperity.bean.ContatoBean;
+
 @Entity
 @Table(name = "tbCandidato")
 public class CandidatoEntity {
@@ -61,6 +63,15 @@ public class CandidatoEntity {
 
 	@Column(name = "cmCurriculo")
 	private File curriculo;
+	
+	@Column(name = "dtUltimoContato")
+	private Date dataultimoContato;
+	
+	@Column(name = "dtEntrevista")
+	private Date dataEntrevista;
+	
+	@Column(name = "dsProposta")
+	private String proposta;
 
 	/* Mapeamento de Relacionamentos */
 
@@ -176,6 +187,30 @@ public class CandidatoEntity {
 		this.curriculo = curriculo;
 	}
 
+	public Date getDataUltimoContato() {
+		return dataultimoContato;
+	}
+
+	public void setDataUltimoContato(Date contatoBean) {
+		this.dataultimoContato = contatoBean;
+	}
+
+	public Date getDataEntrevista() {
+		return dataEntrevista;
+	}
+
+	public void setDataEntrevista(Date dataEntrevista) {
+		this.dataEntrevista = dataEntrevista;
+	}
+
+	public String getProposta() {
+		return proposta;
+	}
+
+	public void setProposta(String proposta) {
+		this.proposta = proposta;
+	}
+
 	public ContatoEntity getContato() {
 		return contato;
 	}
@@ -215,5 +250,5 @@ public class CandidatoEntity {
 	public void setStatusCandidatos(List<StatusCandidatoEntity> statusCandidatos) {
 		this.statusCandidatos = statusCandidatos;
 	}
-
 }
+	
