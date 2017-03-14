6126a3b82cf9aa2cb6be458cd4e79cfb1c4ae367
André Posman <andre.posman@verity.com.br>
2017-03-14 16:58:27 -0300

    Alterações para página de Histórico Candidato e criação de Entities

--- Prosperity/src/main/java/br/com/prosperity/controller/CandidatoController.java
+++ Prosperity/src/main/java/br/com/prosperity/controller/CandidatoController.java
@@ -23,85 +23,83 @@ import br.com.prosperity.business.SituacaoAtualBusiness;
 import br.com.prosperity.business.TipoCursoBusiness;
 
 @Controller
-@RequestMapping(value="candidato")
+@RequestMapping(value = "candidato")
 public class CandidatoController {
-	
+
 	@Autowired
 	private CandidatoBusiness candidatoBusiness;
-	
+
 	@Autowired
 	private EnderecoBean enderecoBean;
-	
+
 	@Autowired
 	private FormacaoBean formacaoBean;
-	
+
 	@Autowired
 	private ContatoBean contatoBean;
 
 	@Autowired
 	private TipoCursoBusiness tipoCursoBusiness;
-	
+
 	@Autowired
 	private SituacaoAtualBusiness situacaoAtualBusiness;
-	
+
 	@Autowired
 	private CargoBusiness cargoBusiness;
-	
-	
+
 	@Autowired
 	private TipoCursoBean tipoCursoBean;
 
 	@Autowired
 	private SenioridadeBusiness senioridadeBusiness;
-	
+
 	@Autowired
 	private SituacaoAtualBean situacaoAtualBean;
 
 	@Autowired
 	private CargoBean cargoBean;
-	
-	
-	@RequestMapping(value ="cadastrar", method = RequestMethod.GET)
+
+	@RequestMapping(value = "cadastrar", method = RequestMethod.GET)
 	public String cadastrarCandidato(Model model) {
 		List<TipoCursoBean> tiposCurso = tipoCursoBusiness.getTipoCurso();
 		model.addAttribute("tiposCurso", tiposCurso);
-		
+
 		List<SituacaoAtualBean> listaSituacaoAtual = situacaoAtualBusiness.getSituacaoAtual();
 		model.addAttribute("listaSituacaoAtual", listaSituacaoAtual);
-		
+
 //		List<CargoBean> listaCargo = cargoBusiness.getCargo();
 //		model.addAttribute("listaCargo", listaCargo);
-		
-		List<SenioridadeBean> listaSenioridade = senioridadeBusiness.getSenioridade();
-		model.addAttribute("listaSenioridade", listaSenioridade);
-		
+
+//		List<SenioridadeBean> listaSenioridade = senioridadeBusiness.getSenioridade();
+//		model.addAttribute("listaSenioridade", listaSenioridade);
+
 		return "candidato/cadastrar-candidato";
 	}
-	
-	@RequestMapping(value ="consultar-rh", method = RequestMethod.GET)
+
+	@RequestMapping(value = "consultar-rh", method = RequestMethod.GET)
 	public String consultarCandidatoRH(Model model) {
 		List<CandidatoBean> candidatos = candidatoBusiness.obterTodos();
 		model.addAttribute("candidatos", candidatos);
 		return "candidato/consulta-rh";
-	
+
 	}
-	
-	@RequestMapping(value ="consultar-gestor", method = RequestMethod.GET)
+
+	@RequestMapping(value = "consultar-gestor", method = RequestMethod.GET)
 	public String consultarCandidatoGestor() {
 		return "candidato/consulta-gestor";
 	}
-	
-	@RequestMapping(value ="historico", method = RequestMethod.GET)
+
+	@RequestMapping(value = "historico", method = RequestMethod.GET)
 	public String historicoCandidato(Model model) {
 		CandidatoBean candidatoBean = new CandidatoBean();
 		candidatoBean = candidatoBusiness.obter(3);
-		
+
 		enderecoBean = candidatoBean.getEndereco();
 		contatoBean = candidatoBean.getContato();
 		formacaoBean = candidatoBean.getFormacao();
 		situacaoAtualBean = formacaoBean.getSituacaoAtualBean();
-		tipoCursoBean = formacaoBean.getTipoCursoBean();		
-		
+		tipoCursoBean = formacaoBean.getTipoCursoBean();
+
 		model.addAttribute("candidato", candidatoBean);
 		model.addAttribute("endereco", enderecoBean);
 		model.addAttribute("contato", contatoBean);
@@ -112,7 +110,6 @@ public class CandidatoController {
 		model.addAttribute("candidato", candidatoBean);
 		model.addAttribute("endereco", enderecoBean);
 
-		
 		return "candidato/historico-candidato";
 	}
 }
