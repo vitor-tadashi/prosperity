4cbb4a7cb37d880d6310a9579611f940cd4328ba
VERITY\guilherme.oliveira <guilherme.oliveira@verity.com.br>
2017-03-24 11:08:14 -0300

    commit

--- Prosperity/src/main/java/br/com/prosperity/bean/AvaliadorBean.java
+++ Prosperity/src/main/java/br/com/prosperity/bean/AvaliadorBean.java
@@ -1,21 +1,10 @@
 package br.com.prosperity.bean;
 
-import java.io.File;
-
 import org.springframework.stereotype.Component;
 
 @Component
 public class AvaliadorBean {
 	private Integer idAvaliador;
-	
-	public AvaliadorBean() {
-		
-	}
-	
-	public AvaliadorBean(Integer idAvaliador) {
-		super();
-		this.idAvaliador = idAvaliador;
-	}
 
 	public Integer getIdAvaliador() {
 		return idAvaliador;
@@ -24,6 +13,5 @@ public class AvaliadorBean {
 	public void setIdAvaliador(Integer idAvaliador) {
 		this.idAvaliador = idAvaliador;
 	}
-	
-	
+
 }
