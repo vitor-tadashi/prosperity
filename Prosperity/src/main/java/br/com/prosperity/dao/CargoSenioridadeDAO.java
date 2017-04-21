95188a920eafc86a47cf69af82fc43d2a33311d4
Fabio Vilani Peres <fabio.peres@verity.local>
2017-04-21 17:48:40 -0300

    Alterações de lay-outs na JSP, javascript para verificar o telefone e alterar a mascara conforme celular ou telefone fixo, retirada de mascara do telefone para salvar no banco de dados, ajax para puxar o range salarial, acrescentado tab de marketing com ckeditor mas ainda não está salvando, criado converter e DAO que não existia.

---
+++ Prosperity/src/main/java/br/com/prosperity/dao/CargoSenioridadeDAO.java
@@ -0,0 +1,11 @@
+package br.com.prosperity.dao;
+
+import java.io.Serializable;
+
+import org.springframework.stereotype.Repository;
+import br.com.prosperity.entity.CargoSenioridadeEntity;
+
+@Repository
+public class CargoSenioridadeDAO extends GenericDAOImpl<CargoSenioridadeEntity, Serializable> {
+
+}
