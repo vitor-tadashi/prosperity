6a75a933a81af9ac1d499f162727f12a7047f81d
Daniel Valle <daniel.valle@verity.local>
2017-05-11 16:03:15 -0300

    Inserindo status SEM VAGA ao cancelar a vaga atrelada a Ele

--- Prosperity/src/main/java/br/com/prosperity/business/CandidatoBusiness.java
+++ Prosperity/src/main/java/br/com/prosperity/business/CandidatoBusiness.java
@@ -339,7 +339,8 @@ public class CandidatoBusiness {
 		List<StatusDisponivelEntity> statusDisponivelEntity = statusDisponivelDAO.findAll();
 
 		if (situacaoCandidato.getStatus().getValue() == StatusCandidatoEnum.CANDIDATURA.getValue()
-				|| situacaoCandidato.getStatus().getValue() == StatusCandidatoEnum.CANCELADO.getValue()) {
+				|| situacaoCandidato.getStatus().getValue() == StatusCandidatoEnum.CANCELADO.getValue()
+				|| situacaoCandidato.getStatus().getValue() == StatusCandidatoEnum.SEMVAGA.getValue()) {
 			statusCandidatoEntity = statusAlteracao(situacaoCandidato);
 			statusCandidatoDAO.insert(statusCandidatoEntity);
 			buscarUsuariosParaEmail(situacaoCandidato);
