abbaabd61ea5c5b15a867dd8107323cef254adc7
LeoRamos01 <leonardo.ramos@verity.local>
2017-05-15 16:46:14 -0300

    ComunicacaoEntity e Bean

--- Prosperity/src/main/java/br/com/prosperity/entity/ComunicacaoEntity.java
+++ Prosperity/src/main/java/br/com/prosperity/entity/ComunicacaoEntity.java
@@ -1,5 +1,78 @@
 package br.com.prosperity.entity;
 
-public class ComunicacaoEntity {
+import java.util.Date;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tbComunicacao")
+public class ComunicacaoEntity {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "idComunicacaos", unique = true, nullable = false)
+	private Integer id;
+	
+	@Column(name = "dtContato")
+	private Date dataContato;
+	
+	@Column(name = "dsObservacao")
+	private String observacao;
+	
+	@ManyToOne(cascade = CascadeType.REFRESH , fetch = FetchType.LAZY)
+	@JoinColumn(name = "idUsuario")
+	private UsuarioEntity usuario;
+	
+	@ManyToOne(cascade = CascadeType.REFRESH , fetch = FetchType.LAZY)
+	@JoinColumn(name = "idCandidato")
+	private CandidatoEntity candidato;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Date getDataContato() {
+		return dataContato;
+	}
+
+	public void setDataContato(Date dataContato) {
+		this.dataContato = dataContato;
+	}
+
+	public String getObservacao() {
+		return observacao;
+	}
+
+	public void setObservacao(String observacao) {
+		this.observacao = observacao;
+	}
+
+	public UsuarioEntity getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(UsuarioEntity usuario) {
+		this.usuario = usuario;
+	}
+
+	public CandidatoEntity getCandidato() {
+		return candidato;
+	}
+
+	public void setCandidato(CandidatoEntity candidato) {
+		this.candidato = candidato;
+	}
 }
