3df74371907da10500361de4fcd8ac9d252c183a
Daniel Valle <daniel.valle@verity.local>
2017-05-16 10:34:50 -0300

    Configurando as classes para dataContato

--- Prosperity/src/main/java/br/com/prosperity/bean/ComunicacaoBean.java
+++ Prosperity/src/main/java/br/com/prosperity/bean/ComunicacaoBean.java
@@ -1,5 +1,44 @@
 package br.com.prosperity.bean;
 
+import java.sql.Date;
+
 public class ComunicacaoBean {
+	
+	private Integer id;
+	private Date dataContato;
+	private String oberservacao;
+	private UsuarioBean usuarioBean;
+	private CandidatoBean candidatoBean;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Date getDataContato() {
+		return dataContato;
+	}
+	public void setDataContato(Date dataContato) {
+		this.dataContato = dataContato;
+	}
+	public String getOberservacao() {
+		return oberservacao;
+	}
+	public void setOberservacao(String oberservacao) {
+		this.oberservacao = oberservacao;
+	}
+	public UsuarioBean getUsuarioBean() {
+		return usuarioBean;
+	}
+	public void setUsuarioBean(UsuarioBean usuarioBean) {
+		this.usuarioBean = usuarioBean;
+	}
+	public CandidatoBean getCandidatoBean() {
+		return candidatoBean;
+	}
+	public void setCandidatoBean(CandidatoBean candidatoBean) {
+		this.candidatoBean = candidatoBean;
+	}
 
 }
--- Prosperity/src/main/java/br/com/prosperity/entity/ComunicacaoEntity.java
+++ Prosperity/src/main/java/br/com/prosperity/entity/ComunicacaoEntity.java
@@ -1,5 +1,28 @@
 package br.com.prosperity.entity;
 
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tbComunicacao")
 public class ComunicacaoEntity {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id", unique = true, nullable = false)
+	private Integer id;
+	
+	@Column(name = "dtContato")
+	private Date dataContato;
+	
+	@Column(name = "dsObervacao")
+	private String observacao;
+	
 }
