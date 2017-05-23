e74b8515abdf40f15c34c6a51025c8a96c86c0f4
VERITY\guilherme.oliveira <guilherme.oliveira@verity.com.br>
2017-05-23 15:30:32 -0300

    correção.

--- Prosperity/src/main/java/br/com/prosperity/dao/CancelamentoDAO.java
+++ Prosperity/src/main/java/br/com/prosperity/dao/CancelamentoDAO.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Repository;
 
 import br.com.prosperity.entity.CancelamentoEntity;
 
-
 @Repository
 public class CancelamentoDAO extends GenericDAOImpl<CancelamentoEntity, Integer> {
 }
